046e3a0d2075b8847e84dee16d07b9d5e9d6b287
Henrik <henrinsk@student.matnat.uio.no>
2023-12-18 21:39:36 +0100

    Part 2 is working I think, but takes more than 12 hours to execute. Haven't come up with a way to fix it yet other than parallellising, which is still at least 2 hours.

--- Day 8 (Python)/8.py
+++ Day 8 (Python)/8.py
@@ -6,30 +6,54 @@ def move(graph, cur, instruction):
     else:
         return graph[cur][0]
 
+def ghostMove(graph, cur, instruction):
+    newCur = []
+    #writing two loops is unaesthetic but saves us from thousands of if checks
+    if instruction == "R":
+        for pos in cur:
+            newCur.append(graph[pos][1])
+    else:
+        for pos in cur:
+            newCur.append(graph[pos][0])
+    return newCur
+
+def ghostDone(cur):
+    for pos in cur:
+        if pos[-1] != "Z":
+            return False
+    return True
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Please provide a filepath and a task number.")
     else:
         with open(sys.argv[1]) as infile:
-            if sys.argv[2] == "1":
+            mode = sys.argv[2]
+            instructions = infile.readline()[:-1]
+            infile.readline() #skip blank line
+            graph = {}
+            count = 0
+            index = 0
+            instructionLength = len(instructions)
+            if mode == "2":
+                cur = []
+                
+            for line in infile:
+                #lines are constant length
+                #this means we can just use indices directly
+                origin = line[:3]
+                left = line[7:10]
+                right = line[12:15]
+                graph[origin] = (left,right)
+                if mode == "2" and origin[-1] == "A":
+                    cur.append(origin)
+                    
+            if mode == "1":
                 print("Running task 1.")
-                instructions = infile.readline()[:-1]
-                infile.readline() #skip blank line
-                graph = {}
-                for line in infile:
-                    #lines are constant length
-                    #this means we can just use indices directly
-                    origin = line[:3]
-                    left = line[7:10]
-                    right = line[12:15]
-                    graph[origin] = (left,right)
                 cur = "AAA"
-                count = 0
-                index = 0
-                instructionLength = len(instructions)
                 while cur != "ZZZ":
                     count += 1
-                    tmp = cur
+                    #tmp = cur
                     cur = move(graph, cur, instructions[index])
                     #print(f"Moved from {tmp} to {cur}.")
                     index += 1
@@ -40,7 +64,18 @@ if __name__ == "__main__":
                     
                     
 
-            elif sys.argv[2] == "2":
+            elif mode == "2":
                 print("Running task 2.")
+                print(cur)
+                while not ghostDone(cur):
+                    count += 1
+                    if count % 1000000000 == 0:
+                        print(count)
+                    cur = ghostMove(graph, cur, instructions[index])
+
+                    index += 1
+                    if index >= instructionLength:
+                        index = 0
+                print(f"Result: {count}")
                 
                 
