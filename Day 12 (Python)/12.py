f068974cc5bb34b6e7a1ce76e75203cda4a72979
Henrik <henrinsk@student.matnat.uio.no>
2023-12-22 15:29:35 +0100

    Work on 12-1. Recursion like this is hard.

--- Day 12 (Python)/12.py
+++ Day 12 (Python)/12.py
@@ -3,49 +3,51 @@ import sys
 def clamp(mn, x, mx):
     return max(mn, min(x,mx))
 
-def findPossibleSequences(row):
-    independentSequences = []
-    sequence = ""
-    for i in range(len(row)):
-        if row[i] == "?" or row[i] == "#":
-            sequence += row[i]
-            print(sequence)
-        elif row[i] == ".":
-            if sequence != "":
-                independentSequences.append(sequence)
-            sequence = ""
-    if sequence != "":
-        independentSequences.append(sequence)
-    return independentSequences
-
-def mappable(index, potential, actual):
-    #check if actual can map onto potential starting at index
-    if index + actual == len(potential):
-        if index == 0:
-            return True
-        else:
-            if potential[index-1] == "#":
-                return False
-            else:
-                return True
-    if index + actual > len(potential):
+def mappable(index, value, target):
+    #check if we can start at this index with this value
+    if index+value >= len(target):
         return False
     if index > 0:
-        if potential[index-1] == "#":
+        if target[index-1] == "#":
             return False
-    if potential[index+actual] == "#":
-        return False
+    for i in range(index,index+value - 1):
+        if target[i] != "#" or target[i] != "?":
+            return False
     return True
-    
-                
 
-def mapCountSingle(potential, actual):
-    #finds amount of ways a single actual value can map onto a potential one
+def mapAll(row, values):
     count = 0
-    for i in range(len(potential)):
-        if mappable(i, potential, actual):
-            count += 1
+    for i in range(len(row)):
+        count += mapRecursive(i, row, values, count)
+        print(f"Collected a total of {count} ways to map onto {row} at {i}")
     return count
+
+def mapRecursive(index, row, values, count):
+    print(values)
+    print(index)
+    if values == []:
+        return count + 1
+    if index + values[0] > len(row):
+        if len(values) != 1 or values[0] != 1:
+            return count
+        if row[-1] == "#" or row[-1] == "?":
+            return count + 1
+    if row[index] == "#" or row[index] == "?":
+        if values[0] > 1:
+            values[0] -= 1
+        else:
+            if index < len(row) - 1:
+                if row[index+1] != "#":
+                    index += 1
+                    if len(values) > 1:
+                        values = values[1:]
+                    else:
+                        values = []
+    return mapRecursive(index+1, row, values, count)
+
+        
+
+                            
             
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -59,12 +61,13 @@ if __name__ == "__main__":
                     line = line.split(" ")
                     gears = line[0]
                     sequences = line[1].split(",")
-                    potentialSequences = findPossibleSequences(gears)
-                    for potential in potentialSequences:
-                        for actual in sequences:
-                            actual = int(actual)
-                            count = mapCountSingle(potential, actual)
-                            print(f"Found {count} ways to map {actual} onto {potential}.")
+                    print(sequences)
+                    sequences = list(sequences)
+                    for i in range(len(sequences)):
+                        sequences[i] = int(sequences[i])
+                    print(sequences)
+                    mapAll(gears, sequences)
+                    break
 
 
             elif sys.argv[2] == "2":
